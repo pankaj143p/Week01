10880c710547a79733b7be784b0936e6c69ca7c4
pankaj143p <pankaj114477pankaj@gmail.com>
2025-01-17 12:29:32 +0530

    extras all problems added

--- OppsProblems/Level_01/Book.java
+++ OppsProblems/Level_01/Book.java
@@ -1,17 +1,17 @@
 // create a class Book with the following private attributes
 class Book{
     private String title;
-    private String auther;
+    private String author;
     private int price;
-    Book(String title, String auther, int price){
+    Book(String title, String author, int price){
         this.title=title;
-        this.auther=auther;
+        this.author=author;
         this.price=price;
     // create a method to display the book details
     }
     private void display(){
         System.out.println("Book Title: "+title);
-        System.out.println("Book Auther: "+auther);
+        System.out.println("Book author: "+author);
         System.out.println("Book Price: "+price);
 
     }   
